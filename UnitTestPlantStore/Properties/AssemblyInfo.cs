d6b8c5929f4729fbb53437b17938f62edf5eb54a
khuynh0101 <khuynh0101@gmail.com>
2020-09-13 14:17:11 -0400

    Add unit testing on store

---
+++ UnitTestPlantStore/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPlantStore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestPlantStore")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f4544d2-c846-4be5-a5bd-f1781b3e7f58")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestPlantStore/UnitTestStore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlantStoreAPI.Repositories;
+
+namespace UnitTestPlantStore
+{
+    [TestClass]
+    public class UnitTestStore
+    {
+        [TestMethod]
+        public void TestGetMenu_ShouldReturnMenuItems()
+        {
+            MenuRepository menuRepo = new MenuRepository();
+            Assert.IsTrue(menuRepo.GetMenu().Count > 0);
+        }
+
+        [TestMethod]
+        public void TestGetAllProducts_ShouldReturnAllProducts()
+        {
+            ProductRepository productRepo = new ProductRepository();
+            Assert.IsTrue(productRepo.GetAllProducts().Count > 0);
+        }
+    }
+}
